5ba73310ee0471cb157fd0505420d7eba74a321d
Sergio Real <sreagon@upv.edu.es>
2025-10-16 20:53:36 +0200

    Añadido código de test de la clase Node; Makefile actualizado

--- Node.h
+++ Node.h
@@ -5,7 +5,10 @@ class Node {
     public:
         T data;
         Node<T>* next;
-        Node(T data, Node<T>* next=nullptr);
+        Node(T data, Node<T>* next=nullptr){
+            this->data = data;
+            this->next = next;
+        }
         friend std::ostream& operator<<(std::ostream &out, const Node<T> &node){
             out << node.data;
             return out;
